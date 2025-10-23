4f84ef8c3ac6ecec6fcbda456c878b36735a732e
FilipRajdl <filipf.rajdl@gmail.com>
2025-10-23 09:05:22 +0000

    23.10.2025

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+std::string cisla[9] = {"one","two","three","four","five","six","seven","eight","nine"};
+
+int preved_na_cislo(std::string retezec, int pozice){
+    for(int i=0;i<9;i++){
+        if(retezec.find(cisla[i], pozice)==pozice){
+            return i+1;
+        }
+    }
+    return 0;
+}
+
+int main(){
+
+    std::ifstream soubor("data.txt");
+    std::string radek;
+    int suma = 0;
+    if (!soubor.is_open()) {
+        std::cout << "Soubor se nepodarilo otevrit pro cteni." << std::endl;
+    }else{
+        while(std::getline(soubor, radek)){
+            std::string cislice;
+            for(int i=0;i<radek.length();i++){
+                if(isdigit(radek[i])){
+                    cislice+=radek[i];
+                    break;
+                }
+                int cislo = preved_na_cislo(radek,i);
+                if(cislo!=0){
+                    cislice+=std::to_string(cislo);
+                    break;
+                }
+            }
+            for(int i=radek.length()-1;i>=0;i--){
+                if(isdigit(radek[i])){
+                    cislice+=radek[i];
+                    break;
+                }
+                int cislo = preved_na_cislo(radek,i);
+                if(cislo!=0){
+                    cislice+=std::to_string(cislo);
+                    break;
+                }
+            }
+            suma+=std::stoi(cislice);
+        }
+
+        std::cout<<suma<<std::endl;
+
+    }
+    return 0;
+}
